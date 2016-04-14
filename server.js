3b04b212baf266caf6a763d4720b2db3b70d0580
Matt Corbett <mcorbett@gmx.co.uk>
2016-04-14 22:45:44 +0100

    new1 add todos and todos id

--- server.js
+++ server.js
@@ -1,28 +1,18 @@
 var express = require('express');
 var app = express();
+var bodyparser = require('body-parser');
 var PORT = process.env.PORT || 3000;
-var todos =[{
-    id: 1,
-    description: 'Meet Mum for lunch',
-    completed: false
-},{
-    id: 2,
-    description: 'Go to market',
-    boolean: false
-},{
-    id:3,
-    description: 'Feed Tia',
-    boolean: true
-},{
-    id:4,
-    description: 'Feed Ralphy as well',
-    boolean: true
-}];
+var todos =[];
+var todoNextId=1;
 
+app.use(bodyparser.json());
+
+// GET /todos
 app.get('/todos', function (req, res){
     res.json(todos)
 });
 
+// GET /todos/:id
 app.get('/todos/:id', function (req, res) {
     var todoId=parseInt(req.params.id, 10);
     todos.forEach (function (todo) {
@@ -36,6 +26,14 @@ app.get('/todos/:id', function (req, res) {
 
 app.get('/', function(req, res) {
     res.send('Todo Api Root');
+});
+
+//POST /todos
+app.post('/todos', function (req, res) {
+    var body = req.body;
+    
+    console.log('description ' + body.description);
+    res.json(body);
 });
 
 app.listen(PORT, function () {
