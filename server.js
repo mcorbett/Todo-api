283ef4f435fd130eeb48bf9819dc7238782bb2f4
Matt Corbett <mcorbett@gmx.co.uk>
2016-04-27 21:55:03 +0100

    changed server.js and package.json files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
+var _ = require('underscore');
 var PORT = process.env.PORT || 3000;
 var todos =[];
 var todoNextId=1;
@@ -15,13 +16,13 @@ app.get('/todos', function (req, res){
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
     var todoId=parseInt(req.params.id, 10);
-    todos.forEach (function (todo) {
-        if (todoId === todo.id) {
-            res.json(todo)
-            exit;
-        }
-    });
-    res.status(404).send()
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    if (matchedTodo) {
+        res.json(matchedTodo);
+    }
+    else {
+        res.status(404).send();
+    }
 });
 
 app.get('/', function(req, res) {
@@ -31,6 +32,12 @@ app.get('/', function(req, res) {
 //POST /todos
 app.post('/todos', function (req, res) {
     var body = req.body;
+// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0)
+    if // (!_.isBoolean(body.completed) ||
+    (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        console.log("check failed " + body.completed);
+        return res.status(400).send();
+    }
     body.id = todoNextId++;
     todos.push(body);
     // console.log('description ' + body.description);
