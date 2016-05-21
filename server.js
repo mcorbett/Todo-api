12f2db68996dac0f5988d5cb44db218f124e3280
Matt Corbett <mcorbett@gmx.co.uk>
2016-05-21 21:04:32 +0100

    Added Query to /todos

--- server.js
+++ server.js
@@ -13,20 +13,39 @@ app.use(bodyparser.json());
 // GET /todos
 app.get('/todos', function (req, res){
     var query = req.query;
-    var filteredTodos = todos;
-    if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-        filteredTodos = _.where(filteredTodos, {completed: true});
-    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-        filteredTodos = _.where(filteredTodos, {completed: false});
+    var where = {};
+
+    if (query.hasOwnProperty('completed') && query.completed === 'true') {
+        where.completed = true;
+    } else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+        where.completed = false;
     }
 
-    if (queryParams.hasOwnProperty('q') &&  queryParams.q.length > 0) {
-        filteredTodos = _.filter(filteredTodos,function(todo) {
-            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-        });
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.description = {
+           $like: '%' + query.q + '%'
+        };
     }
+    db.todo.findAll({where: where}).then(function (todos) {
+        res.json(todos);
+    }, function (e) {
+        res.status(500).send();
+    })
 
-    res.json(filteredTodos);
+    // var filteredTodos = todos;
+    // if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+    //     filteredTodos = _.where(filteredTodos, {completed: true});
+    // } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+    //     filteredTodos = _.where(filteredTodos, {completed: false});
+    // }
+    //
+    // if (queryParams.hasOwnProperty('q') &&  queryParams.q.length > 0) {
+    //     filteredTodos = _.filter(filteredTodos,function(todo) {
+    //         return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+    //     });
+    // }
+    //
+    // res.json(filteredTodos);
 });
 
 // GET /todos/:id
