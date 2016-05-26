3e36f522da6283ff6afbbe0fab70e2a822fc4d01
Matt Corbett <mcorbett@gmx.co.uk>
2016-05-27 00:19:34 +0100

    Added delete to server.js

--- server.js
+++ server.js
@@ -80,16 +80,22 @@ app.post('/todos', function (req, res) {
 
 app.delete('/todos/:id', function (req, res) {
     var todoId=parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
-    if (matchedTodo) {
-        todos = _.without(todos,matchedTodo);
-        res.json(matchedTodo);
-        return res.status(200).send();
-    }
-    else {
-        res.status(404).send();
-    }
+
+    db.todo.destroy({
+        where: {id: todoId}
+    }).then(function (rowsDeleted) {
+        if (rowsDeleted === 0) {
+            res.status(404).json({
+                error: 'No todo with that id'
+            });
+        } else {
+            res.status(204).send()
+        }
+    }, function () {
+        res.status(500).send();
+    });
 });
+
 
 app.put('/todos/:id', function (req, res) {
     var todoId=parseInt(req.params.id, 10);
