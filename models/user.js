583b1edbbd2a59b57cd12d7a9ff8cb7ae922aee9
mcorbett <mcorbett@gmx.co.uk>
2016-07-21 16:12:26 +0100

    new version

--- models/user.js
+++ models/user.js
@@ -1,19 +1,32 @@
 module.exports = function(sequelize, DateTypes) {
-    return sequelize.define('user', {
-        email: {
-            type: DateTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true
-            }
-        },
-        password: {
-            type:DateTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [7, 100]
-            }
+  return sequelize.define('user', {
+    email: {
+      type: DateTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type:DateTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [7, 100]
+      }
+    }
+  },{
+    hooks: {
+      beforeValidate: function(user, options) {
+        if (typeof user.email === 'string') {
+          user.email = user.email.toLowerCase();
+        } else {
+          console.log('Not String ');
         }
-    });
+      },
+      afterValidate: function(user, options) {
+
+      }
+    }
+  });
 }
