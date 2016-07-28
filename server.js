2cc7b517e8a6ccd5ab49aba980ca76b7fbe24e89
Matt Corbett <mcorbett@gmx.co.uk>
2016-07-28 08:38:49 +0100

    /usr/login finds by email address and password2

--- server.js
+++ server.js
@@ -96,20 +96,12 @@ app.post('/users', function(req, res) {
 
 app.post('/users/login', function(req, res) {
     var body = _.pick(req.body, 'email','password');
-    if (typeof body.email !== 'string' || typeof body.password !== 'string') {
-        return res.status(400).send();
-    }
-
-        db.user.findOne({ where: {email: body.email} }).then(function (user) {
-            if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
-                console.log('got here');
-                return res.status(401).send();
-            }
-            res.json(user.toPublicJSON());
-        }, function (e) {
-            res.status(500).send();
-        });
+    db.user.authenticate(body).then(function (user) {
+        res.json(user.toPublicJSON());
+    }, function (e) {
+        res.status(401).send();
     });
+});
 
     app.put('/todos/:id', function (req, res) {
         var todoId=parseInt(req.params.id, 10);
