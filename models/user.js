d5d620c9b1522dd5a0cc7c55a007269e73d450cd
Matt Corbett <mcorbett@gmx.co.uk>
2016-07-30 18:06:36 +0100

    To show kurt

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
-
+var cryptojs=require('crypto-js');
+var jwt = require('jsonwebtoken');
 module.exports = function(sequelize, DataTypes) {
     var user = sequelize.define('user', {
         email: {
@@ -55,7 +56,6 @@ module.exports = function(sequelize, DataTypes) {
                         if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
                             console.log('failed compare');
                             return reject();
-
                         }
                         resolve(user);
                     }, function(e) {
@@ -69,6 +69,26 @@ module.exports = function(sequelize, DataTypes) {
             toPublicJSON: function() {
                 var json = this.toJSON();
                 return _.pick(json, 'email', 'id', 'createdAt', 'updatedAt');
+            },
+            generateToken: function (type) {
+                if (_.isString(type)) {
+                    return undefined;
+                }
+                try {
+                    var stringData = JSON.stringify({
+                        id: this.get('id'), type: type
+                    });
+                    var encryptedData = cryptjs.AES.encrypt(stringData, 'abc123!@#!').toString();
+                    var token = jwt.sign({
+                        token: encryptedData
+                    }, 'qwert98');
+
+                    return token;
+                } catch (e) {
+                    return undefined;
+
+                }
+
             }
         }
     });
